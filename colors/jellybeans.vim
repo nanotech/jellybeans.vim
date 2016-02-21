nanotech/jellybeans.vim

commit 2b423cf7d9d300ea7dee70fac081aeb44c186007

Allow transparency on common Linux terminals
by setting ctermbg to NONE wherever
g:jellybeans_background_color is used.

This also removes
g:jellybeans_background_color_256 as no background
color is set on 256-color terminals now.

Fixes #39.

colors/jellybeans.vim
@@ -65,17 +65,12 @@ endif
 
 " Configuration Variables:
 " - g:jellybeans_background_color
-" - g:jellybeans_background_color_256
 " - g:jellybeans_overrides
 " - g:jellybeans_use_lowcolor_black
 " - g:jellybeans_use_term_italics
 
 if !exists("g:jellybeans_background_color")
   let g:jellybeans_background_color = "151515"
-end
-
-if !exists("g:jellybeans_background_color_256")
-  let g:jellybeans_background_color_256 = 233
 end
 
 if !exists("g:jellybeans_use_lowcolor_black") || g:jellybeans_use_lowcolor_black
@@ -297,12 +292,18 @@ fun! s:X(group, fg, bg, attr, lcfg, lcbg)
     let l:fge = empty(a:fg)
     let l:bge = empty(a:bg)
 
+    if a:bg == g:jellybeans_background_color
+      let l:ctermbg = 'NONE'
+    else
+      let l:ctermbg = s:rgb(a:bg)
+    endif
+
     if !l:fge && !l:bge
-      exec "hi ".a:group." guifg=#".a:fg." guibg=#".a:bg." ctermfg=".s:rgb(a:fg)." ctermbg=".s:rgb(a:bg)
+      exec "hi ".a:group." guifg=#".a:fg." guibg=#".a:bg." ctermfg=".s:rgb(a:fg)." ctermbg=".l:ctermbg
     elseif !l:fge && l:bge
       exec "hi ".a:group." guifg=#".a:fg." guibg=NONE ctermfg=".s:rgb(a:fg)." ctermbg=NONE"
     elseif l:fge && !l:bge
-      exec "hi ".a:group." guifg=NONE guibg=#".a:bg." ctermfg=NONE ctermbg=".s:rgb(a:bg)
+      exec "hi ".a:group." guifg=NONE guibg=#".a:bg." ctermfg=NONE ctermbg=".l:ctermbg
     endif
   endif
 
@@ -548,10 +549,7 @@ if !s:low_color
   hi CursorColumn ctermbg=234
   hi CursorLine ctermbg=234
   hi SpecialKey ctermbg=234
-  exec "hi NonText ctermbg=".g:jellybeans_background_color_256
-  exec "hi LineNr ctermbg=".g:jellybeans_background_color_256
   hi DiffText ctermfg=81
-  exec "hi Normal ctermbg=".g:jellybeans_background_color_256
   hi DbgBreakPt ctermbg=53
   hi IndentGuidesOdd ctermbg=235
   hi IndentGuidesEven ctermbg=234

commit b01398984f11ae4da2cad9462dc5794993dabc9e

More accurate 256-color mapping for greys
Fixes #49, a regression caused in 2b423cf by
removing the 256 background color override. Some
other previously overridden colors are now mapped
accurately, so their overrides have been removed.

colors/jellybeans.vim
@@ -268,6 +268,11 @@ endfun
 
 " returns the palette index to approximate the given R/G/B color levels
 fun! s:color(r, g, b)
+  " map greys directly (see xterm's 256colres.pl)
+  if &t_Co == 256 && a:r == a:g && a:g == a:b
+    return float2nr(round(a:r - 8) / 10.0) + 232
+  endif
+
   " get the closest grey
   let l:gx = s:grey_number(a:r)
   let l:gy = s:grey_number(a:g)
@@ -578,11 +583,6 @@ call s:X("PreciseJumpTarget","B9ED67","405026","","White","Green")
 if !s:low_color
   hi StatusLineNC ctermbg=235
   hi Folded ctermbg=236
-  hi FoldColumn ctermbg=234
-  hi SignColumn ctermbg=236
-  hi CursorColumn ctermbg=234
-  hi CursorLine ctermbg=234
-  hi SpecialKey ctermbg=234
   hi DiffText ctermfg=81
   hi DbgBreakPt ctermbg=53
   hi IndentGuidesOdd ctermbg=235